a744d003226314496c6c660abd7e05da2a0a5e59
AndreiPetrusMihai <makerVD@gmail.com>
2021-10-31 10:57:57 +0200

    Reimplemented the parsing scanner.

--- flcd-symTable/src/main/java/Main.java
+++ flcd-symTable/src/main/java/Main.java
@@ -7,13 +7,17 @@ public class Main {
     public static void main(String[] args) {
 
         String[] fileList = {
-                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\p1.txt",
-                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\p2.txt",
-                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\p3.txt",
-                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\p1err.txt"};
+                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\programs\\p1.txt",
+                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\programs\\p2.txt",
+                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\programs\\p3.txt",
+                "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\programs\\p1err.txt"
+                };
+
+        String separatorsPath = "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\separators.txt";
+        String reservedWordsPath = "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\reservedWords.txt";
 
         try {
-            FileScanner scanner = new FileScanner( "W:\\Year3Sem1\\FLCD\\flcd-andrei-petrus\\tokens.txt");
+            FileScanner scanner = new FileScanner( separatorsPath, reservedWordsPath);
             for(String file : fileList){
                 scanner.setFilePath(file);
                 scanner.processFile();
--- flcd-symTable/src/main/java/Scanner/FileScanner.java
+++ flcd-symTable/src/main/java/Scanner/FileScanner.java
@@ -11,39 +11,67 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class FileScanner {
 
-    private String filePath;
-    private String tokensPath;
+
+    private String separatorsPath;
+    private List<String> separators = new ArrayList<>();
+
+    private String reservedWordsPath;
+    private List<String> reservedWords = new ArrayList<>();
+
     private List<String> reserved = new ArrayList<>();
+
+    Pattern reservedTokenPattern;
+
+    private String filePath;
 
     private SymbolTable symbolTable = new SymbolTable();
     private List<PifEntry> pifTable = new ArrayList<>();
 
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+    public FileScanner(String separatorsPath, String reservedWordsPath) {
+        this.separatorsPath = separatorsPath;
+        this.reservedWordsPath = reservedWordsPath;
+        generateReservedLists();
+        generatePattern();
     }
 
-    public FileScanner(String tokensPath) {
-        this.tokensPath = tokensPath;
-        generateReservedList();
+    private void generateReservedLists(){
+        fromFileToArray(separators,separatorsPath);
+        fromFileToArray(reservedWords, reservedWordsPath);
+        reserved.addAll(reservedWords);
+        reserved.addAll(separators);
     }
 
-    private void generateReservedList(){
+    private void fromFileToArray(List<String> array, String filePath){
         BufferedReader reader;
         try{
-            reader = new BufferedReader(new FileReader(tokensPath));
+            reader = new BufferedReader(new FileReader(filePath));
             String token = reader.readLine();
             while(token != null){
-                reserved.add(token);
+                array.add(token);
                 token = reader.readLine();
             }
             reader.close();
         } catch(IOException e){
             e.printStackTrace();
         }
+    }
+
+    private void generatePattern(){
+        String separatorsGroup = "(";
+        for(String reservedWord : reservedWords){
+            separatorsGroup = separatorsGroup.concat("(\\b"+ Pattern.quote(reservedWord)  +"\\b)|");
+        }
+        for(String separator : separators){
+            separatorsGroup = separatorsGroup.concat("("+ Pattern.quote(separator)  +")|");
+        }
+        separatorsGroup = separatorsGroup.substring(0, separatorsGroup.length()-1);
+        separatorsGroup+= ")";
+        //System.out.println(separatorsGroup);
+        reservedTokenPattern = Pattern.compile(separatorsGroup);
     }
 
     private boolean isValidString(String token){
@@ -62,24 +90,58 @@ public class FileScanner {
         return token.matches("(^[1-9]\\d*\\.\\d\\d*$)|(^0$)");
     }
 
-    private boolean isValidConstant(String token){
+    private boolean isValidNonStringConstant(String token){
         return isValidInteger(token) || isValidFloat(token);
+    }
+
+    private void processStringToken(String sToken,Integer lNumber,Integer tNumber){
+        if(isValidString(sToken)){
+            Pair<Integer, Integer> st_pos = symbolTable.pos(sToken);
+            pifTable.add(new PifEntry("constant", st_pos));
+        } else {
+            System.out.println("Lexical error");
+            System.out.println("Error on line " + lNumber + ", token " + tNumber);
+            return;
+        }
     }
 
     private void processToken(String token) throws Exception {
         for(String reservedToken : reserved){
             if(token.equals(reservedToken)){
-                //Removed the token
                 pifTable.add(new PifEntry(reservedToken, new Pair<>(-1,-1)));
                 return;
             }
         }
-        if(isValidConstant(token) || isValidIdentifier(token)){
+        if(isValidIdentifier(token)){
             Pair<Integer, Integer> st_pos = symbolTable.pos(token);
-            pifTable.add(new PifEntry(token, st_pos));
+            pifTable.add(new PifEntry("identifier", st_pos));
+            return;
+        }
+        if(isValidNonStringConstant(token)){
+            Pair<Integer, Integer> st_pos = symbolTable.pos(token);
+            pifTable.add(new PifEntry("constant", st_pos));
             return;
         }
         throw new Exception("Invalid sequence of charachters.");
+    }
+
+    private List<String> generateTokensList(String section){
+        List<String> tokenList = new ArrayList<>();
+        //Split by space in order to simplify the process.
+        List<String>  concatenatedTokens = Arrays.stream(section.split(" ")).filter(str -> str.length() > 0).toList();
+        for(String concatToken : concatenatedTokens){
+            Matcher matcher = reservedTokenPattern.matcher(concatToken);
+            int lastMatchIndex = 0;
+            while(matcher.find()){
+                if(lastMatchIndex != matcher.start()){
+                    //if the next reserved token doesn't start where the last one ended, we have an indentifier or a constant
+                    tokenList.add(concatToken.substring(lastMatchIndex,matcher.start()));
+                }
+                lastMatchIndex = matcher.end();
+                tokenList.add(matcher.group());
+            }
+        }
+        return tokenList;
     }
 
     public void processFile() {
@@ -89,7 +151,6 @@ public class FileScanner {
         java.util.Scanner scanner;
         File file = new File(filePath);
         try{
-
             scanner = new java.util.Scanner(file);
             //Split the fiels line by line
             int lineNumber = 0;
@@ -106,27 +167,22 @@ public class FileScanner {
                 }
                 int tokenNumber = 0;
                 for(int index = 0; index < stringSplits.size() ; index++){
-
                     String section = stringSplits.get(index);
                     if(index%2 == 1){
                         //We are in a string constant and we should just put it in the pif/ST
-                        if(isValidString(section)){
-                            Pair<Integer, Integer> st_pos = symbolTable.pos(section);
-                            pifTable.add(new PifEntry(section, st_pos));
-                        } else {
-                            System.out.println("Lexical error");
-                            System.out.println("Error on line " + lineNumber + ", token " + tokenNumber);
-                            return;
-                        }
+                        processStringToken(section, lineNumber, tokenNumber);
                         tokenNumber++;
 
                     } else {
-                        //Continue processing
-                        List<String>  sectionTokens = Arrays.stream(section.split(" ")).filter(str -> str.length() > 0).toList();
-                        for(String token : sectionTokens){
+                        //COnstruct a list that contains the separated tokens
+                        List<String> tokens = generateTokensList(section);
+
+                        for(String token : tokens){
                             try{
-                                processToken(token);
+                                //Classify and process each token
+                                processToken(token.trim());
                             } catch(Exception e){
+                                System.out.println(token);
                                 System.out.println("Lexical error");
                                 System.out.println("Error on line " + lineNumber + ", token " + tokenNumber);
                                 return;
@@ -136,10 +192,8 @@ public class FileScanner {
                     }
                 }
                 lineNumber++;
-
             }
             scanner.close();
-
         } catch(FileNotFoundException e){
             e.printStackTrace();
         }
@@ -176,4 +230,10 @@ public class FileScanner {
         stWriter.flush();
         stWriter.close();
     }
+
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
 }
